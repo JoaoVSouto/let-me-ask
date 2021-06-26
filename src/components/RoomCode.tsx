b97766661d62190b7135bfa7557e2a6a8f63f8a7
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-26 14:07:57 -0300

    Create add to clipboard functionality on room code button

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -2,14 +2,26 @@ import copyImg from 'assets/images/copy.svg';
 
 import 'styles/room-code.scss';
 
-export default function RoomCode() {
+type RoomCodeProps = {
+  code: string;
+};
+
+export default function RoomCode({ code }: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(code);
+  }
+
   return (
-    <button type="button" className="room-code">
+    <button
+      type="button"
+      className="room-code"
+      onClick={copyRoomCodeToClipboard}
+    >
       <span className="room-code__copy-icon-container">
         <img src={copyImg} alt="Copiar código da sala" />
       </span>
 
-      <span className="room-code__label">Sala #-Md7Vwzgqh1uf1UUtq9F</span>
+      <span className="room-code__label">Sala #{code}</span>
     </button>
   );
 }
