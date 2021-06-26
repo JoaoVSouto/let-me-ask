4e16094dcb1a9f021106949585c52f15864712c5
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-26 09:44:49 -0300

    Add logic for creating a new room

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,9 @@
-import { Link } from 'react-router-dom';
+import * as React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import { database } from 'services/firebase';
+
+import { useAuth } from 'contexts/AuthContext';
 
 import illustrationImg from 'assets/images/illustration.svg';
 import logoImg from 'assets/images/logo.svg';
@@ -8,6 +13,29 @@ import Button from 'components/Button';
 import 'styles/auth.scss';
 
 export default function NewRoom() {
+  const { user } = useAuth();
+
+  const history = useHistory();
+
+  const [newRoom, setNewRoom] = React.useState('');
+
+  async function handleCreateRoom(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -23,8 +51,13 @@ export default function NewRoom() {
         <div className="main-content">
           <img src={logoImg} alt="let me ask" />
           <h2>Crie uma nova sala</h2>
-          <form>
-            <input type="text" placeholder="Nome da sala" />
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              value={newRoom}
+              onChange={e => setNewRoom(e.target.value)}
+            />
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Home from 'pages/Home';
 import NewRoom from 'pages/NewRoom';
+import Room from 'pages/Room';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/rooms/new" component={NewRoom} />
+      <Route path="/rooms/:id" component={Room} />
 
       <Redirect to="/" />
     </Switch>
