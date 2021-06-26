5c85dd94b13cbaf97464258d99b6326f4622db3c
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-26 11:22:24 -0300

    Structure and style room header and form

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,3 +1,38 @@
+import logoImg from 'assets/images/logo.svg';
+
+import Button from 'components/Button';
+
+import 'styles/room.scss';
+
 export default function Room() {
-  return <h1>Room page</h1>;
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Let me ask" />
+
+          <div>codigo</div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala React Q&A</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+          <textarea placeholder="O que você quer perguntar?" />
+
+          <div className="form-footer">
+            <span>
+              Para enviar uma pergunta,{' '}
+              <button type="button">faça seu login</button>.
+            </span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }
